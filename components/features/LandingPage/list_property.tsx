66f4a606287732cfb7f6b7aab794c89129324134
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2024-09-17 06:46:13 +0000

    Refactor ListProperty component to use Link for listing a property

--- components/features/LandingPage/list_property.tsx
+++ components/features/LandingPage/list_property.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 export default function ListProperty() {
   return (
     <div className="mb-8">
-      <button className="w-full bg-orange-500 text-white font-semibold py-3 rounded-md hover:bg-orange-600 transition duration-300">
-        List your property
-      </button>
+      <Link href="/add">
+        <button className="w-full bg-orange-500 text-white font-semibold py-3 rounded-md hover:bg-orange-600 transition duration-300">
+          List your property
+        </button>
+      </Link>
     </div>
   )
 }
